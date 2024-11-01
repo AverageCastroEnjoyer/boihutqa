2e73ceffce3ef0fe3965b399d9b3fb306b850430
Santiago <santiramos.cr@gmail.com>
2024-11-01 15:50:22 -0600

    maybe runnable

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/santy/Documentos/QA/ProyectoQA/boihut/env"
+set -gx VIRTUAL_ENV "/home/santy/Documentos/QA/boihutqa/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
