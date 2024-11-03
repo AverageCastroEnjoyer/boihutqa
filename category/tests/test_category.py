951853daba6ff4f590181136a411108aa3f9aaa5
manfredjones <110132001+manfredjones@users.noreply.github.com>
2024-11-03 15:27:53 -0600

    Update test_category.py

--- category/tests/test_category.py
+++ category/tests/test_category.py
@@ -1,87 +1,85 @@
-# boihut/category/tests/test_category.py
-
 from django.test import TestCase
 from category.models import Category
 from django.core.exceptions import ValidationError
+from django.core.files.uploadedfile import SimpleUploadedFile
 
-class CategoryModelTests(TestCase):
-    
+class CategoryTestCase(TestCase):
+
     def setUp(self):
-        # Set up two sample categories
-        self.category1 = Category.objects.create(
+        self.category = Category.objects.create(
             category_name="Fiction",
             slug="fiction",
-            category_des="Category for fiction books"
-        )
-        self.category2 = Category.objects.create(
-            category_name="Non-Fiction",
-            slug="non-fiction",
-            category_des="Category for non-fiction books"
+            category_image=SimpleUploadedFile("category_image.jpg", b"file_content", content_type="image/jpeg"), 
+            category_des="A category for fictional books."
         )
 
-    def test_category_creation(self):
-        """Test if a category is created successfully"""
-        self.assertEqual(self.category1.category_name, "Fiction")
-        self.assertEqual(self.category1.slug, "fiction")
-    
-    def test_category_str_method(self):
-        """Test the __str__ method of Category model"""
-        self.assertEqual(str(self.category1), "Fiction")
-    
-    def test_category_slug_unique(self):
-        """Test that the slug field is unique"""
+    def test_create_category(self):
+        self.assertEqual(self.category.category_name, "Fiction")
+        self.assertEqual(self.category.slug, "fiction")
+        self.assertTrue(self.category.category_image.url.startswith('/media/images/cat/'))
+        self.assertEqual(self.category.category_des, "A category for fictional books.")
+
+    def test_category_string_representation(self):
+        self.assertEqual(str(self.category), "Fiction")
+
+    def test_unique_category_slug(self):
         with self.assertRaises(Exception):
             Category.objects.create(
-                category_name="Fiction",
-                slug="fiction"
+                category_name="Another Fiction",
+                slug="fiction",  
             )
 
-    def test_category_name_max_length(self):
-        """Test that category_name cannot exceed 100 characters"""
-        long_name = "a" * 101
-        category = Category(category_name=long_name, slug="long-name-slug")
-        with self.assertRaises(ValidationError):
-            category.full_clean()  # This will raise ValidationError if the max_length is exceeded
+    def test_update_category(self):
+        self.category.category_name = "Updated Fiction"
+        self.category.save()
+        self.assertEqual(self.category.category_name, "Updated Fiction")
 
+    def test_delete_category(self):
+        category_id = self.category.id
+        self.category.delete()
+        with self.assertRaises(Category.DoesNotExist):
+            Category.objects.get(id=category_id)
 
-    def test_category_slug_max_length(self):
-        """Test that slug cannot exceed 20 characters"""
-        long_slug = "a" * 21
-        category = Category(category_name="Test Category", slug=long_slug)
-        with self.assertRaises(ValidationError):
-            category.full_clean()
-
-    def test_category_image_blank_allowed(self):
-        """Test if category_image can be blank"""
-        category = Category.objects.create(
-            category_name="Science",
-            slug="science"
+    def test_category_description_max_length(self):
+        long_description = 'B' * 2001  
+        category = Category(
+            category_name="Long Description Category",
+            slug="long-description-category",
+            category_des=long_description  
         )
-        self.assertEqual(category.category_image.name, "")
+        with self.assertRaises(ValidationError):
+            category.full_clean()  
 
-    def test_category_description_optional(self):
-        """Test if category description can be blank"""
-        category = Category.objects.create(
-            category_name="History",
-            slug="history"
+    def test_category_image_upload(self):
+        new_category = Category.objects.create(
+            category_name="Non-Fiction",
+            slug="non-fiction",
+            category_image=SimpleUploadedFile("non_fiction_image.jpg", b"file_content", content_type="image/jpeg"),
+            category_des="A category for non-fictional books."
         )
-        self.assertEqual(category.category_des, "")
+        self.assertTrue(new_category.category_image.url.startswith('/media/images/cat/'))
 
+    def test_create_category_with_blank_slug(self):
+        with self.assertRaises(ValidationError):
+            category = Category(
+                category_name="Category with Blank Slug",
+                slug="",  
+                category_des="A category with a blank slug."
+            )
+            category.full_clean() 
+            
+    def test_category_retrieve_by_slug(self):
+        category = Category.objects.get(slug="fiction")
+        self.assertEqual(category, self.category)
+            
     def test_category_unique_name(self):
-        """Test that the category_name is unique"""
         with self.assertRaises(Exception):
             Category.objects.create(
                 category_name="Fiction",
                 slug="new-fiction"
             )
-
-    def test_category_retrieve_by_slug(self):
-        """Test retrieving a category by its slug"""
-        category = Category.objects.get(slug="fiction")
-        self.assertEqual(category, self.category1)
-
+   
     def test_category_update_description(self):
-        """Test updating the description of a category"""
-        self.category1.category_des = "Updated description"
-        self.category1.save()
+        self.category.category_des = "Updated description"
+        self.category.save()
         self.assertEqual(Category.objects.get(slug="fiction").category_des, "Updated description")
